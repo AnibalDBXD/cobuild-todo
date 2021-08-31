53ba6689a3f1ba1fad291bbdfc84a1031db96125
Anibal Olivares <olivaresluisanibal@gmail.com>
2021-08-31 21:30:43 +0000

    Add Login with google Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import { Stack, Heading, Button } from "@chakra-ui/react";
 import type { NextPage } from 'next';
+import { useRouter } from "next/router";
+import Image from 'next/image';
+import googleIcon from "../icons/g-normal.png";
 const Home: NextPage = () => {
+  const { push } = useRouter();
   return (
-    <Stack>
-      <Heading>Login with</Heading>
-      <Button>Google</Button>
+    <Stack alignSelf="center" marginTop="30%" spacing="1rem" width="60%">
+      <Heading textAlign="center">Login with</Heading>
+      <Button
+        backgroundColor="white"
+        color="black"
+        colorScheme="whiteAlpha"
+        leftIcon={<Image alt="Google" src={googleIcon} />}
+        onClick={(): void =>{ push("/todo"); }}
+      >Google
+      </Button>
     </Stack>
   );
 };
