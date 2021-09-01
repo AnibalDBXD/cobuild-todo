6eae449c9591d40b26b667b95049b03fcce504ac
Anibal Olivares <olivaresluisanibal@gmail.com>
2021-09-01 15:53:01 +0000

    Can edit on click, styles

--- components/Todos/Item/index.tsx
+++ components/Todos/Item/index.tsx
@@ -1,8 +1,10 @@
-import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { GridItem, Grid, Checkbox, IconButton, Text } from "@chakra-ui/react";
+import { CloseIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { GridItem, Grid, Checkbox, IconButton, Text, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { IItem } from "../../../interfaces/todo";
 
 const Item = ({ complete, text }: IItem): JSX.Element => {
+  const [isEditing, setEditing] = useState(false);
   return (
     <Grid backgroundColor={`${complete ? "gray.300" : "transparent"}`}
       border="2px"
@@ -19,21 +21,34 @@ const Item = ({ complete, text }: IItem): JSX.Element => {
       >
         <Checkbox isChecked={complete} size="lg" />
       </GridItem>
-      <GridItem colSpan={4}>
-        <Text
-          as="h3"
-          fontSize="1.5rem"
-          fontWeight="bold"
-          textAlign="center"
-        >{text}
-        </Text>
+      <GridItem colSpan={4}
+        fontSize="1.5rem"
+        fontWeight="bold"
+        textAlign="center"
+      >
+        {
+          isEditing ?
+            <Input
+              fontSize="1.5rem"
+              fontWeight="bold"
+              placeholder="Edit task"
+              textAlign="center"
+            />
+            : 
+            <Text
+              as="h3"
+              onDoubleClick={(): void => setEditing(true)}
+            >{text}
+            </Text>
+        }
       </GridItem>
       <GridItem colSpan={1}>
         <IconButton
           aria-label="Edit"
           colorScheme="gray"
           fontSize="20px"
-          icon={<EditIcon />}
+          icon={isEditing ? <CloseIcon /> :<EditIcon />}
+          onClick={(): void => setEditing(cv => !cv)}
           variant="outline"
         />
       </GridItem>
