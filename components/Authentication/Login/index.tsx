b2b181588bfdddb6c5b540f8696762e6d17149cc
Anibal Olivares <olivaresluisanibal@gmail.com>
2021-09-02 03:51:58 +0000

    Can login by username and password

--- components/Authentication/Login/index.tsx
+++ components/Authentication/Login/index.tsx
@@ -1,14 +1,21 @@
+import { Alert, AlertIcon } from "@chakra-ui/react";
 import { Form, Formik, Field, FieldProps } from "formik";
+import { useState } from "react";
+import { loginWithEmail } from "../../../firebase";
 import FormButton from "../FormButton";
 import FormInput from "../FormInput";
 import { validateEmail, validatePassword } from "../utils";
 
 const Login = (): JSX.Element => {
+  const [errorMessage, setErrorMessage] = useState("");
   return (
     <Formik
       initialValues={{ email: '', password: '' }}
-      onSubmit={(values, { setSubmitting }): void => {
+      onSubmit={({ email, password }, { setSubmitting }): void => {
         setSubmitting(true);
+        loginWithEmail(email, password)
+          .catch(({ message }) => setErrorMessage(message))
+          .finally(() => setSubmitting(false));
       }}
     >
       {({
@@ -16,6 +23,12 @@ const Login = (): JSX.Element => {
         isSubmitting,
       }): JSX.Element => (
         <Form onSubmit={handleSubmit}>
+          {errorMessage && (
+            <Alert marginBottom="1rem" status="error">
+              <AlertIcon />
+              {errorMessage}
+            </Alert>
+          )}
           <Field component={FormInput} name="email" validate={validateEmail} />
           <Field
             component={(props: FieldProps): JSX.Element =>
